84cf2bbadcacef2f58339b684fb96c15ad9d9d7c
Leon Wun <leonwun.code@gmail.com>
2024-06-05 23:33:48 +0800

    completed logic for rps game

--- index.js
+++ index.js
@@ -1,116 +1,87 @@
 console.log("Hello World!");
-
-// create a variable, with array containing rock, paper, scissors
-const choice = ["Rock", "Paper", "Scissors"];
-// write function "getComputerChoice"
-// function RANDOMLY return rock, paper, scissors, rememeber value be in STRINGS
-// utilize Math.random to randomly return the choice
-function getComputerChoice() {
-  const CompChoice = choice[Math.floor(Math.random() * 3)];
-  CompChoice.toUpperCase();
-  return CompChoice;
-}
-
-// console.log(getComputerChoice());
-
-// create new function "getHumanChoice"
-// Create prompt message requesting user to input the choices
-// I need to somehow store the STRING value of user input into the function and return "HumanChoice"
-
-// to modify playround function from typing to respond to click
-const rockBtn = document.querySelector(".rockBtn");
-const paperBtn = document.querySelector(".paperBtn");
-const sciBtn = document.querySelector(".sciBtn");
-
-function getHumanChoice() {
-  const HumanChoice = prompt("Rock? Paper? Scissors?");
-  return HumanChoice;
-}
-
-// console.log(getHumanChoice());
-
-// Create scoring variables
+const gameButton = document.querySelectorAll(".gameBtn");
+const result = document.getElementById("result");
+const choice = ["ROCK", "PAPER", "SCISSORS"];
+let humanScoreDisplay = document.getElementById("humanScore");
+let computerScoreDisplay = document.getElementById("computerScore");
 let humanScore = 0;
 let computerScore = 0;
 
-// Create single round function named "playRound"
-// Takes 2 parameter: humanChoice, computerChoice
-// Make the function case insensitive, google how to do it
-// Write the logic using if else statement and logical operators
-// console.log the result of WIN/LOSE with string value
-// Increment the score based on the outcome
+function getComputerChoice() {
+  const CompChoice = choice[Math.floor(Math.random() * 3)];
+  return CompChoice;
+}
 
+function updateScoreDisplay() {
+  humanScoreDisplay.textContent = `Human: ${humanScore}`;
+  computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+}
+
+function disableButtons() {
+  gameButton.forEach((btn) => (btn.disabled = true));
+}
+
+function checkForWinner() {
+  if (humanScore == 5) {
+    result.textContent = "You Win!";
+    disableButtons();
+  } else if (computerScore == 5) {
+    result.textContent = "Computer Win!";
+    disableButtons();
+  }
+}
+
+// use the method where condition not met it will exit and repeat the loop
 function playRound(humanChoice, computerChoice) {
   switch (true) {
     // Human Wins
     // Case Rock Wins
     case humanChoice === "ROCK" && computerChoice === "SCISSORS":
       humanScore++;
-      console.log("You Win! Rock Smashes Scissors!");
+      result.textContent = "You Win! Rock Smashes Scissors!";
       break;
     // Case Paper Wins
     case humanChoice === "PAPER" && computerChoice === "ROCK":
       humanScore++;
-      console.log("You Win! Paper wrapped Rock!");
+      result.textContent = "You Win! Paper wrapped Rock!";
       break;
     // Case Scissors Wins
     case humanChoice === "SCISSORS" && computerChoice === "PAPER":
       humanScore++;
-      console.log("You Win! Scissors cut Paper!");
+      result.textContent = "You Win! Scissors cut Paper!";
       break;
     // Computer Wins
     // Case Rock Wins
     case humanChoice === "SCISSORS" && computerChoice === "ROCK":
       computerScore++;
-      console.log("You Lose! Rock Smashes Scissors!");
+      result.textContent = "You Lose! Rock Smashes Scissors!";
       break;
     // Case Paper Wins
     case humanChoice === "ROCK" && computerChoice === "PAPER":
       computerScore++;
-      console.log("You Lose! Paper wrapped Rock!");
+      result.textContent = "You Lose! Paper wrapped Rock!";
       break;
     // Case Scissors Wins
     case humanChoice === "PAPER" && computerChoice === "SCISSORS":
       computerScore++;
-      console.log("You Lose! Scissors cut Paper!");
+      result.textContent = "You Lose! Scissors cut Paper!";
       break;
     // Case Draw
     case humanChoice === computerChoice:
-      console.log("It's a tie!");
+      result.textContent = "It's a tie!";
       break;
     default:
       console.log("Did you type the correct stuff?");
   }
+
+  updateScoreDisplay();
+  checkForWinner();
 }
 
-rockBtn.addEventListener("click", playRound);
-paperBtn.addEventListener("click", playRound);
-sciBtn.addEventListener("click", playRound);
-
-// create entire game with variable "playGame"
-// need to play 5 round, so probably LOOP 5 times
-// move "playRound" inside "playGame"
-/*
-function playGame() {
-  for (let round = 1; round < 6; round++) {
-    let humanSelection = getHumanChoice().toUpperCase();
-    let computerSelection = getComputerChoice().toUpperCase();
-
-    console.log("Round " + round);
-    playRound(humanSelection, computerSelection);
-    console.log("You Choose " + humanSelection);
-    console.log("Computer Choose " + computerSelection);
-    console.log("Your Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
-  }
-  if (humanScore > computerScore) {
-    alert("Congratulations! You win against the Computer!");
-  } else if (computerScore > humanScore) {
-    alert("You lose! You failure! EMOTIONAL DAMAGE!!");
-  } else if (humanScore === computerScore) {
-    alert("It's a tie!");
-  }
-}
-
-playGame();
-*/
+gameButton.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    let humanChoice = e.target.textContent;
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
+});
